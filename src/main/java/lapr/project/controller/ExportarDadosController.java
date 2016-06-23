59dae2ff54845e4252ca876d28cd834e55e5f1ae
João Vaz <1150617@isep.ipp.pt>
2016-06-23 12:04:52 +0100

    Criação das classes referentes ao uc28 - ExportarDados. [UC28][Implementation]

---
+++ src/main/java/lapr/project/controller/ExportarDadosController.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lapr.project.controller;
+
+import java.io.File;
+import lapr.project.model.CentroExposicoes;
+import lapr.project.model.EscreverXML;
+
+/**
+ *
+ * @author miniondevil
+ */
+public class ExportarDadosController {
+   private final CentroExposicoes ce;
+   public ExportarDadosController(CentroExposicoes ce){
+       this.ce=ce;
+   }
+   public void escreverFicheiro(File ficheiro){
+    EscreverXML escrever = new EscreverXML(ce,ficheiro);   
+    escrever.escreverCentro();
+    
+   }
+}
