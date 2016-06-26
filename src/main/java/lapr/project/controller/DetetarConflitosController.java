4e796242e0646e2a53306c2cdbbbce040deeca66
marianachilro <1141537@isep.ipp.pt>
2016-06-26 06:32:46 +0100

    Elaboração de teste unitários ao controller do UC13.[Implementation][UC-13][fixes issue #14][Unit Test]

--- src/main/java/lapr/project/controller/DetetarConflitosController.java
+++ src/main/java/lapr/project/controller/DetetarConflitosController.java
@@ -38,4 +38,7 @@ public class DetetarConflitosController {
             }
         }  
     }
+    
+    
+
 }
---
+++ src/test/java/lapr/project/controller/DetetarConflitosControllerTest.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lapr.project.controller;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author marianachilro
+ */
+public class DetetarConflitosControllerTest {
+    
+    public DetetarConflitosControllerTest() {
+    }
+
+    /**
+     * Test of run method, of class DetetarConflitosController.
+     */
+    @Test
+    public void testRun() {
+        System.out.println("run");
+        DetetarConflitosController instance = null;
+        instance.run();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
